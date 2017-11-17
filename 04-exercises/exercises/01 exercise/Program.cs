9f1b015cd1828ef0a419b0d53188b1e69c95a657
RobiiiOlen <32923360+RobiiiOlen@users.noreply.github.com>
2017-11-17 13:54:19 +0200

    exercises

---
+++ 04-exercises/exercises/01 exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("See on github demosaatmine");
+            Console.WriteLine("Kui palju on 237x68?");
+
+            string text = "56169";
+            int sum = int.Parse(text);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
