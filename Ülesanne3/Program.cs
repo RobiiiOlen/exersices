4ce66bbca334a67f0c7de81012b180f4b7c41667
RobiiiOlen <32923360+RobiiiOlen@users.noreply.github.com>
2018-04-09 14:43:46 +0300

    exam

---
+++ Ülesanne3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta siia minimaalne aastaarv");
+            string a = Console.ReadLine();
+            int min = int.Parse(a);
+
+
+            Console.WriteLine("Sisesta siia maksimaalne aastaarv");
+            string b = Console.ReadLine();
+            int max = int.Parse(b);
+
+
+            Console.WriteLine("Sisesta siia genereeritavate andmete hulk");
+            string c = Console.ReadLine();
+            int quantity = int.Parse(c);
+
+            int d = 0;
+
+            while (d < quantity)
+            {
+                Random arv = new Random();
+                int number = arv.Next(min, max);
+
+                Console.WriteLine(number);
+                d = d + 1;
+
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
