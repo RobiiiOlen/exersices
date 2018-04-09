b8b9e4a7ec2fd85332760167470af92ea764b39c
RobiiiOlen <32923360+RobiiiOlen@users.noreply.github.com>
2018-04-09 14:41:25 +0300

    exam

---
+++ Ülesanne1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nimed = new List<string>();
+            Console.WriteLine("Sisesta nimi suure algustähega!");
+            while (true)
+            {
+
+                Console.Write("-->");
+                string input = Console.ReadLine();
+
+                nimed.Add(input);
+
+                if (input == "-1")
+                {
+                    break;
+                }
+            }
+
+            string[] nime_arr = nimed.ToArray();
+
+            foreach (var value in nimed)
+            {
+                Console.Write($" {value}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
