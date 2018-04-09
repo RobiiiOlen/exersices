cf63bd9f7f01ac196ef00e1e5e2a6bb4a3af5638
RobiiiOlen <32923360+RobiiiOlen@users.noreply.github.com>
2018-04-09 14:43:17 +0300

    exam

---
+++ Ülesanne2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Ülesanne 2
+            Console.WriteLine("Sisesta lauseid, kus on kasutatud järgmisi nimesi:");
+
+            string[] nimed = new string[10] { "kaur", "mattias", "kristel", "heleri", "trevor",
+                                               "kristjan", "kelli", "kevin", "maarika", "laura" };
+
+            {
+                int indeks = 0;
+                while (indeks < nimed.Length)
+                {
+
+                    string nimi = nimed[indeks];
+
+                    string Suurtäht = char.ToUpper(nimi[0]) + nimi.Substring(1).ToLower();
+
+                    Console.Write(Suurtäht);
+
+                    indeks += 1;
+
+                    if (indeks < nimed.Length)
+                    {
+
+                        Console.Write(", ");
+                    }
+
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
